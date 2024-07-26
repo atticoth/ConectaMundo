03611d0e9773d01683dcaee40c3724f08b40b7a7
Thiago Attico <thiagoattico@hotmail.com>
2024-07-26 02:19:31 -0300

    Release 2.0

--- android/app/src/main/java/com/medeirosge/conectamundo/MainApplication.kt
+++ android/app/src/main/java/com/medeirosge/conectamundo/MainApplication.kt
@@ -2,16 +2,13 @@ package com.medeirosge.conectamundo
 
 import android.app.Application
 import android.content.res.Configuration
-
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
@@ -34,9 +31,6 @@ class MainApplication : Application(), ReactApplication {
           override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
   )
-
-  override val reactHost: ReactHost
-    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
